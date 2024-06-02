ebf5485a4eb8e4f6c95c48f6dde0fdca056edc78
iwhore <ihorjaremko17@gmail.com>
2024-06-02 21:52:29 +0200

    v

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -1,6 +1,6 @@
 import { Image } from "../../types";
 import ImageCard from "../ImageCard/ImageCard";
-
+import css from "../ImageGallery/ImageGallery.module.css"; //css
 
 interface ImageGalleryProps {
   listImages: Image[];
@@ -12,10 +12,10 @@ export default function ImageGallery({
   openModal,
 }: ImageGalleryProps) {
   return (
-    <ul>
+    <ul className={css.list}>
       {listImages.map((item) => {
         return (
-          <li key={item.id}>
+          <li className={css.item} key={item.id}>
             <ImageCard onClick={openModal} image={item} />
           </li>
         );
